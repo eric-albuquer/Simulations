ad23481a69ab06e3f6734330e79c8c1af5c7c565
Eric <ega2@cesar.school>
2024-09-09 07:30:25 -0300

    Atualizar o Python

---
+++ Python/Renderização/Ray Casting/main.py
@@ -0,0 +1,147 @@
+import math
+import pygame
+
+ROWS = 10
+COLS = 10
+
+pygame.init()
+
+map = []
+for i in range(ROWS):
+    map.append([])
+    for j in range(COLS):
+        color = 0
+        if i == 0: color = [255, 0, 0]
+        elif j == 0: color = [0, 255, 0]
+        elif i == ROWS - 1: color = [0, 0, 255]
+        elif j == COLS - 1: color = [255, 0, 255]
+
+        map[i].append(color)
+
+info = pygame.display.Info()
+WIDTH = info.current_w
+HEIGHT = info.current_h
+
+CELL_SIZE = WIDTH / 50
+
+def drawMap(screen, cell_size):
+    pygame.draw.rect(screen, (127, 127, 127), (0, 0, cell_size * COLS, cell_size * ROWS))
+    for i in range(ROWS):
+        y = cell_size * i
+        for j in range(COLS):
+            x = cell_size * j
+            color = (0, 0, 0) if map[i][j] == 0 else map[i][j]
+            pygame.draw.rect(screen, color, (x, y, cell_size - 1, cell_size - 1))
+ 
+def drawPlayer(screen, px, py, cell_size):
+    pygame.draw.circle(screen, (255, 255, 0), (px * cell_size, py * cell_size), cell_size / 4)
+    
+def drawRay(screen, ray, px, py, cell_size):
+    x = (ray[1] + px) * cell_size
+    y = (ray[2] + py) * cell_size
+    pygame.draw.line(screen, (255, 0, 0), (px * cell_size, py * cell_size), (x, y), 2)
+
+def raycast(x, y, angle):
+    deltaX = math.cos(angle)
+    deltaY = math.sin(angle)
+    a = abs(math.tan(angle))
+    dist_x = 0
+    dist_y = 0
+    stepX = 1 if deltaX > 0 else -1
+    stepY = 1 if deltaY > 0 else -1
+    d = 1e-15
+    rx, ry = 0, 0
+     
+    while 0 <= x < COLS and 0 <= y < ROWS:
+        if map[int(y + stepY * d)][int(x + stepX * d)] != 0: break
+         
+        if deltaX > 0: rx = int(x + 1) - x
+        else: rx = x - int(x - d)
+        if deltaY > 0: ry = int(y + 1) - y
+        else: ry = y - int(y - d)
+         
+        if a * rx < ry:
+            x += rx * stepX
+            y += a * rx * stepY
+            dist_x += rx
+            dist_y += a * rx
+        else:
+            x += ry / a * stepX
+            y += ry * stepY
+            dist_x += ry / a
+            dist_y += ry
+    
+    value = map[int(y + stepY * d)][int(x + stepX * d)]
+    distance = math.sqrt(dist_x ** 2 + dist_y ** 2)   
+     
+    return [distance, dist_x * stepX, dist_y * stepY, value]
+
+def draw3d(screen, x, y, angle, fov_h, fov_v, max_dist):
+    for i in range(WIDTH):
+        dAlpha = fov_h / WIDTH
+        alpha = angle - fov_h / 2 + i * dAlpha
+        ray = raycast(x, y, alpha)
+        dist = ray[0]
+        visible_dist = dist if dist < max_dist else max_dist
+        corrected_dist = visible_dist * math.cos(alpha - angle)
+        line_h = HEIGHT / (fov_v * corrected_dist)
+        h0 = HEIGHT / 2 - line_h
+        h1 = HEIGHT / 2 + line_h
+        if h0 <= 0: h0 = 0
+        if h1 >= HEIGHT: h1 = HEIGHT
+        shadow = visible_dist / max_dist * 255
+        r, g, b = ray[3]
+        color = (max(r - shadow, 0), max(g - shadow, 0), max(b - shadow, 0))
+        
+        pygame.draw.line(screen, (199, 137, 88), (i, h1), (i, HEIGHT), 1)
+        pygame.draw.line(screen, color, (i, h0), (i, h1), 1)
+        pygame.draw.line(screen, (150, 150, 255), (i, 0), (i, h0), 1)
+
+def playerCanMove(px, py):
+    return map[int(py)][int(px)] == 0
+
+def draw():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    running = True
+    angle = 4
+    px = 4.5
+    py = 4.5
+    dx, dy = 0, 0
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        # Obter as teclas pressionadas
+        keys = pygame.key.get_pressed()
+
+        # Movimentar o jogador com as setas do teclado
+        if keys[pygame.K_LEFT]:
+            angle -= 0.03
+        if keys[pygame.K_RIGHT]:
+            angle += 0.03
+        if keys[pygame.K_UP]:
+            dx = math.cos(angle) * 0.03
+            dy = math.sin(angle) * 0.03
+        if keys[pygame.K_DOWN]:
+            dx = - math.cos(angle) * 0.03
+            dy = - math.sin(angle) * 0.03
+        if keys[pygame.K_ESCAPE]:
+            running = False
+
+        if playerCanMove(px + dx, py + dy):
+            px += dx
+            py += dy
+            dx, dy = 0, 0
+
+        draw3d(screen, px, py, angle, math.pi / 3, 2, ROWS)
+        drawMap(screen, CELL_SIZE)
+        drawPlayer(screen, px, py, CELL_SIZE)
+        ray = raycast(px, py, angle)
+        drawRay(screen, ray, px, py, CELL_SIZE)
+        
+        pygame.display.flip()
+    pygame.quit()
+    
+draw()

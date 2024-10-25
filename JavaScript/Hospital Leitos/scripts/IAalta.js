5f784e2ec7c65946e79b6b75a1844fa6ba04aafb
Eric <ega2@cesar.school>
2024-10-25 07:08:28 -0300

    Create IAalta.js

---
+++ JavaScript/Hospital Leitos/scripts/IAalta.js
@@ -0,0 +1,8 @@
+// Dieta, diurese, evacuacoes, queixa (sinais e sintomas), ciclo sono-virgilia, 
+// Exames fisicos: Sinais vitais (frequencia cardiaca, saturação O2, pressão arterial, Tempo de enchimento capilar, frequencia respiratoria)
+// Alteração de exame físico, Geral, ACV (cardiovascular), AR(respiratorio), ABD(abdomem), Extremidades
+// Antecedentes, Tabagismo, Diabets melitus, Hipertensão, Etilismo
+// Hipotese Diagnostica - "palavra"
+// Medicações em uso - antibioticos, antihipertensivos, 
+// Resultado laboratorial
+// Resultado de imagem

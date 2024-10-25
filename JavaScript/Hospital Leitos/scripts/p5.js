4cda6cea4d719b142df3d9f9b4a5238124780285
Eric <ega2@cesar.school>
2024-10-25 07:05:45 -0300

    Update p5.js

--- JavaScript/Hospital Leitos/scripts/p5.js
+++ JavaScript/Hospital Leitos/scripts/p5.js
@@ -1,9 +1,20 @@
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight)
+    textAlign(CENTER, CENTER)
 }
 
 function drawAndares(hospital) {
-    textAlign(CENTER, CENTER)
+    portugues.forward()
+    if (frameCount % 60 === 0) {
+        increase()
+        portugues.limparLeitos()
+    }
+
+    if (frameCount % 240 === 0) {
+        portugues.prevAlta()
+        portugues.checkPay()
+    }
+
     const andares = hospital.andares
     const salas = hospital.salas
     const leitos = hospital.leitosSala
@@ -14,26 +25,43 @@ function drawAndares(hospital) {
         const x = dx * (i % (Math.floor(andares / 2)))
         const y = dy * Math.floor(i / (andares / 2))
         for (let j = 0; j < salas; j++) {
-            const ddx = dx / 2 
-            const ddy = dy / (salas / 2) 
+            const ddx = dx / 2
+            const ddy = dy / (salas / 2)
 
             const xx = ddx * Math.floor(j / (salas / 2)) + x
             const yy = ddy * (j % (salas / 2)) + y
-            stroke(0, 0, 255)
+            
             for (let k = 0; k < leitos; k++) {
                 const dddx = ddx / leitos
                 const xxx = k * dddx + xx
-                strokeWeight(0.1)
                 const idx = i * salas * leitos + j * leitos + k
-                const tempo = hospital.leitos[idx].previsaoAlta
-                if (hospital.leitos[idx].condicao)
-                    fill(255, 255, 0)
-                else 
+                const cor = colorHours(dataAtual, hospital.leitos[idx].previsaoAlta)
+
+                if (hospital.leitos[idx].reinternacao) {
+                    fill(cor, cor, 0)
+                }
+                else if (hospital.leitos[idx].previsaoAlta) {
+                    fill(0, cor, 0)
+                }
+                else if (hospital.leitos[idx].podePagar()) {
+                    fill(0, 255, 255)
+                }
+                else if (hospital.leitos[idx].prevAlta()) {
+                    fill(0, 0, 255)
+                }
+                else if (hospital.leitos[idx].condicao === Condicao.limpeza) {
+                    fill(255, 0, 255)
+                }
+                else {
                     fill(0)
+                }
+                stroke(0, 0, 255)
+
                 rect(xxx, yy, xxx + dddx, yy + ddy)
                 strokeWeight(1)
                 textSize(12)
-                fill(0, 0, 255)
+                stroke(0)
+                fill(0)
                 text(idx, xxx + dddx / 2, yy + ddy / 2)
                 noFill()
             }
@@ -46,10 +74,13 @@ function drawAndares(hospital) {
         rect(x, y, x + dx, y + dy)
         noStroke()
         fill(255)
-        textSize(20)
+        textSize(30)
         if (i < andares)
             text(`Andar ${i + 1}`, x + dx / 2, y + dy / 2)
     }
+
+    textSize(20)
+    text(dataAtual, 300, 20)
 }
 
 function draw() {

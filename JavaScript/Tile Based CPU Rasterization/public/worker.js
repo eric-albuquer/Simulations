1f2a04160eee449f49c306af9d9804bb7a061bdd
Eric <ega2@cesar.school>
2025-02-07 20:39:17 -0300

    Update worker.js

--- JavaScript/Tile Based CPU Rasterization/public/worker.js
+++ JavaScript/Tile Based CPU Rasterization/public/worker.js
@@ -1,7 +1,19 @@
 let frameBuffer, zBuffer, vertices, colors, screenVertices, screenColors;
 let trianglesBox, done, matrix, triangles;
-let width, height, workerIdx;
+let width, height, workerIdx, centerX, centerY;
 let triangleBatch, boundingBox
+let vIdx, startTidx
+let tIdxArray, startIdxFragment, deltaHFrag, startZIdxRow
+const floatToInt = 0x800000
+
+const p0 = { x: 0, y: 0 }
+
+const box = {
+    minX: 0,
+    maxX: width,
+    minY: 0,
+    maxY: height
+}
 
 class vec3 {
     constructor(x, y, z) {
@@ -17,6 +29,23 @@ class color {
         this.g = g
         this.b = b
     }
+}
+
+const vec = new vec3(0, 0, 0)
+const v0 = new vec3(0, 0, 0)
+const v1 = new vec3(0, 0, 0)
+const v2 = new vec3(0, 0, 0)
+
+const c0 = new color(0, 0, 0)
+const c1 = new color(0, 0, 0)
+const c2 = new color(0, 0, 0)
+
+function max(a, b) {
+    return a > b ? a : b
+}
+
+function min(a, b) {
+    return a < b ? a : b
 }
 
 function dot(m, v) {
@@ -37,11 +66,11 @@ function dot(m, v) {
     }
 }
 
-function insideScreen(v) {
+function outsideScreen(v) {
     return (
-        v.x >= 0 && v.x < width &&
-        v.y >= 0 && v.y < height &&
-        v.z >= -0x3fffffff && v.z <= 0x3fffffff
+        v.x < 0 || v.x >= width ||
+        v.y < 0 || v.y >= height ||
+        v.z < -floatToInt || v.z > floatToInt
     )
 }
 
@@ -49,72 +78,58 @@ function insideScreen(v) {
 function toScreen(v, centerX, centerY) {
     v.x = (v.x + 1) * centerX
     v.y = (v.y + 1) * centerY
-    v.z *= 0x3fffffff
+    v.z *= floatToInt
 }
 
 function vertexShader() {
-    const start = triangleBatch.start
-    const end = triangleBatch.end
-    if (start < end) {
-        const centerX = width >> 1
-        const centerY = height >> 1
+    let idx = vIdx
+    let tIdx = startTidx
 
-        let idx = 3 * start
-        let tIdx = start << 2
+    for (let i = triangleBatch.start; i < triangleBatch.end; i++) {
+        let minX = width
+        let maxX = 0
+        let minY = height
+        let maxY = 0
 
-        const w_1 = width - 1
-        const h_1 = height - 1
+        for (let j = 0; j < 3; j++) {
+            const i0 = triangles[idx++] * 3
+            const i1 = i0 + 1
+            const i2 = i1 + 1
 
-        for (let i = start; i < end; i++) {
-            let minX = w_1
-            let maxX = 0
-            let minY = h_1
-            let maxY = 0
+            vec.x = vertices[i0]
+            vec.y = vertices[i1]
+            vec.z = vertices[i2]
 
-            for (let j = 0; j < 3; j++) {
-                const i0 = triangles[idx++] * 3
-                const i1 = i0 + 1
-                const i2 = i1 + 1
+            dot(matrix, vec)
+            toScreen(vec, centerX, centerY)
 
-                const vec = new vec3(vertices[i0], vertices[i1], vertices[i2])
+            screenVertices[i0] = vec.x
+            screenVertices[i1] = vec.y
+            screenVertices[i2] = vec.z
 
-                dot(matrix, vec)
+            minX = min(vec.x, minX)
+            maxX = max(vec.x, maxX)
+            minY = min(vec.y, minY)
+            maxY = max(vec.y, maxY)
 
-                toScreen(vec, centerX, centerY)
-
-                screenVertices[i0] = vec.x
-                screenVertices[i1] = vec.y
-                screenVertices[i2] = vec.z
-
-                if (vec.x < minX)
-                    minX = vec.x
-                if (vec.x > maxX)
-                    maxX = vec.x
-                if (vec.y < minY)
-                    minY = vec.y
-                if (vec.y > maxY)
-                    maxY = vec.y
-
-                screenColors[i0] = colors[i0] * 255
-                screenColors[i1] = colors[i1] * 255
-                screenColors[i2] = colors[i2] * 255
-            }
-            trianglesBox[tIdx++] = Math.max(0, minX)
-            trianglesBox[tIdx++] = Math.max(0, minY)
-            trianglesBox[tIdx++] = Math.min(w_1, maxX)
-            trianglesBox[tIdx++] = Math.min(h_1, maxY)
+            screenColors[i0] = colors[i0] * 255
+            screenColors[i1] = colors[i1] * 255
+            screenColors[i2] = colors[i2] * 255
         }
+        trianglesBox[tIdx++] = max(0, minX)
+        trianglesBox[tIdx++] = max(0, minY)
+        trianglesBox[tIdx++] = min(width, maxX)
+        trianglesBox[tIdx++] = min(height, maxY)
     }
 
     done[workerIdx] = 1
-    while (done.includes(0)) { }
 }
 
-function intersect(tri, box) {
-    const x1 = Math.max(tri.minX, box.minX)
-    const y1 = Math.max(tri.minY, box.minY)
-    const x2 = Math.min(tri.maxX, box.maxX)
-    const y2 = Math.min(tri.maxY, box.maxY)
+function intersect() {
+    const x1 = max(boundingBox.minX, box.minX)
+    const y1 = max(boundingBox.minY, box.minY)
+    const x2 = min(boundingBox.maxX, box.maxX)
+    const y2 = min(boundingBox.maxY, box.maxY)
 
     if (x1 < x2 && y1 < y2) {
         box.minX = x1
@@ -135,13 +150,13 @@ function isTopLeft(start, end) {
 }
 
 function cross2d(a, b, p) {
-    const ab = {x: b.x - a.x, y: b.y - a.y}
-    const ap = {x: p.x - a.x, y: p.y - a.y}
+    const ab = { x: b.x - a.x, y: b.y - a.y }
+    const ap = { x: p.x - a.x, y: p.y - a.y }
 
     return ab.x * ap.y - ab.y * ap.x
 }
 
-function rasterize(v0, v1, v2, c0, c1, c2, box) {
+function rasterize() {
     const area = cross2d(v0, v1, v2)
 
     if (area < 0)
@@ -161,7 +176,8 @@ function rasterize(v0, v1, v2, c0, c1, c2, box) {
     const bias1 = isTopLeft(v2, v0) ? 0 : -0.0001
     const bias2 = isTopLeft(v0, v1) ? 0 : -0.0001
 
-    const p0 = { x: box.minX + 0.5, y: box.minY + 0.5 }
+    p0.x = box.minX + 0.5
+    p0.y = box.minY + 0.5
 
     let w0Row = cross2d(v1, v2, p0) + bias0
     let w1Row = cross2d(v2, v0, p0) + bias1
@@ -169,12 +185,12 @@ function rasterize(v0, v1, v2, c0, c1, c2, box) {
 
     let idxRow = box.minY * width + box.minX
 
-    for (let y = box.minY; y <= box.maxY; y++) {
+    for (let y = box.minY; y < box.maxY; y++) {
         let idx = idxRow
         let w0 = w0Row
         let w1 = w1Row
         let w2 = w2Row
-        for (let x = box.minX; x <= box.maxX; x++) {
+        for (let x = box.minX; x < box.maxX; x++) {
 
             const isInside = w0 >= 0 && w1 >= 0 && w2 >= 0
 
@@ -212,6 +228,30 @@ function rasterize(v0, v1, v2, c0, c1, c2, box) {
     }
 }
 
+function fragmentShader() {
+    let idxRow = startIdxFragment
+    let zIdxRow = startZIdxRow
+
+    for (let y = boundingBox.minY; y < boundingBox.maxY; y++) {
+        let idx = idxRow
+        let zIdx = zIdxRow
+        for (let x = boundingBox.minX; x < boundingBox.maxX; x++) {
+            const alpha = frameBuffer[idx + 3]
+            if (alpha === 0) {
+                const yH = (y / height)
+                frameBuffer[idx] = (1 - yH) * 100 + 255 * yH
+                frameBuffer[idx + 1] = (1 - yH) * 171 + 255 * yH
+                frameBuffer[idx + 2] = (1 - yH) * 221 + 255 * yH
+                frameBuffer[idx + 3] = 255
+            }
+            zIdx++
+            idx += 4
+        }
+        idxRow += deltaHFrag
+        zIdxRow += width
+    }
+}
+
 self.onmessage = (event) => {
     if (event.data.init) {
         frameBuffer = new Uint8ClampedArray(event.data.sharedFrameBuffer);
@@ -223,12 +263,23 @@ self.onmessage = (event) => {
         trianglesBox = new Uint32Array(event.data.sharedTrianglesBox);
         done = new Uint8Array(event.data.sharedDone);
         matrix = new Float32Array(event.data.sharedMatrix);
-        triangles = new Uint16Array(event.data.sharedTriangles);
+        triangles = new Uint32Array(event.data.sharedTriangles);
         width = event.data.width
         height = event.data.height
         workerIdx = event.data.workerIdx
         triangleBatch = event.data.triangleBatch
         boundingBox = event.data.boundingBox
+        centerX = width >> 1
+        centerY = height >> 1
+        vIdx = 3 * triangleBatch.start
+        startTidx = triangleBatch.start << 2
+        deltaHFrag = width << 2
+        startZIdxRow = boundingBox.minY * width + boundingBox.minX
+        startIdxFragment = (boundingBox.minY * width + boundingBox.minX) << 2
+        tIdxArray = new Uint32Array(triangles.length)
+        for (let i = 0; i < triangles.length; i++) {
+            tIdxArray[i] = triangles[i] * 3
+        }
         return;
     }
 
@@ -236,34 +287,38 @@ self.onmessage = (event) => {
 
     let idxBox = 0
 
-    for (let i = 0; i < triangles.length; i += 3) {
-        const i0 = triangles[i] * 3
-        const i1 = triangles[i + 1] * 3
-        const i2 = triangles[i + 2] * 3
+    let i = 0
 
-        const box = {
-            minX: trianglesBox[idxBox++],
-            minY: trianglesBox[idxBox++],
-            maxX: trianglesBox[idxBox++],
-            maxY: trianglesBox[idxBox++],
-        }
+    while (done.includes(0)) { }
 
-        if (!intersect(boundingBox, box))
+    while (i < triangles.length) {
+        const i0 = tIdxArray[i++]
+        const i1 = tIdxArray[i++]
+        const i2 = tIdxArray[i++]
+
+        box.minX = trianglesBox[idxBox++]
+        box.minY = trianglesBox[idxBox++]
+        box.maxX = trianglesBox[idxBox++]
+        box.maxY = trianglesBox[idxBox++]
+
+        if (!intersect())
             continue
 
-        const v0 = new vec3(screenVertices[i0], screenVertices[i0 + 1], screenVertices[i0 + 2])
-        const v1 = new vec3(screenVertices[i1], screenVertices[i1 + 1], screenVertices[i1 + 2])
-        const v2 = new vec3(screenVertices[i2], screenVertices[i2 + 1], screenVertices[i2 + 2])
+        v0.x = screenVertices[i0]; v0.y = screenVertices[i0 + 1]; v0.z = screenVertices[i0 + 2];
+        v1.x = screenVertices[i1]; v1.y = screenVertices[i1 + 1]; v1.z = screenVertices[i1 + 2];
+        v2.x = screenVertices[i2]; v2.y = screenVertices[i2 + 1]; v2.z = screenVertices[i2 + 2];
 
-        if (!insideScreen(v0) && !insideScreen(v1) && !insideScreen(v2))
+        if (outsideScreen(v0) && outsideScreen(v1) && outsideScreen(v2))
             continue
 
-        const c0 = new color(screenColors[i0], screenColors[i0 + 1], screenColors[i0 + 2])
-        const c1 = new color(screenColors[i1], screenColors[i1 + 1], screenColors[i1 + 2])
-        const c2 = new color(screenColors[i2], screenColors[i2 + 1], screenColors[i2 + 2])
+        c0.r = screenColors[i0]; c0.g = screenColors[i0 + 1]; c0.b = screenColors[i0 + 2];
+        c1.r = screenColors[i1]; c1.g = screenColors[i1 + 1]; c1.b = screenColors[i1 + 2];
+        c2.r = screenColors[i2]; c2.g = screenColors[i2 + 1]; c2.b = screenColors[i2 + 2];
 
-        rasterize(v0, v1, v2, c0, c1, c2, box)
+        rasterize()
     }
+
+    fragmentShader()
 
     self.postMessage(true)
 }

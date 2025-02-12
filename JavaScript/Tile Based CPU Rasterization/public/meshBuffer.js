68fdcee66909481f0b0d83836904ff25e3051eba
Eric <ega2@cesar.school>
2025-02-12 14:33:19 -0300

    Update meshBuffer.js

--- JavaScript/Tile Based CPU Rasterization/public/meshBuffer.js
+++ JavaScript/Tile Based CPU Rasterization/public/meshBuffer.js
@@ -1,19 +1,35 @@
-const meshVertices = [
+let trianglesLen = 0
+const maxTriangleCapacity = 500_000
 
-]
+let verticesOffset = 0
+let colorsOffset = 0
+let trianglesOffset = 0
 
-const meshColors = [
+const sharedTriangles = new SharedArrayBuffer(maxTriangleCapacity * 3 * 4)
+const sharedVertices = new SharedArrayBuffer(maxTriangleCapacity * 9 * 4)
+const sharedColors = new SharedArrayBuffer(maxTriangleCapacity * 9 * 4)
+const triangles = new Uint32Array(sharedTriangles)
+const vertices = new Float32Array(sharedVertices)
+const colors = new Float32Array(sharedColors)
 
-]
+function chunkMatrix() {
+    return Array.from({ length: 16 }, () => Array.from({ length: 256 }, () => new Uint32Array(16).fill(0)))
+}
 
-const meshTriangles = [
-
-]
+const chunks = new Map()
 
 function createCube(x, y, z) {
-    meshVertices.push(
+    const startX = Math.floor(x / 16), startZ = Math.floor(z / 16)
+    const chunkKey = `${startX},${startZ}`
+
+    if (!chunks.has(chunkKey)) {
+        chunks.set(chunkKey, chunkMatrix())
+    }
+    chunks.get(chunkKey)[Math.abs(x) % 16][y][Math.abs(z) % 16] = 1 | (trianglesLen << 1)
+
+    vertices.set(
         //Frente
-        x, y, z + 1,
+        [x, y, z + 1,
         x, y + 1, z + 1,
         x + 1, y, z + 1,
         x + 1, y + 1, z + 1,
@@ -46,12 +62,14 @@ function createCube(x, y, z) {
         x, y, z,
         x, y, z + 1,
         x + 1, y, z,
-        x + 1, y, z + 1
-    )
+        x + 1, y, z + 1]
+    , verticesOffset)
 
-    meshColors.push(
+    verticesOffset += 72
+
+    colors.set(
         //Frente
-        0, 0, 1,
+        [0, 0, 1,
         0, 1, 1,
         1, 0, 1,
         1, 1, 1,
@@ -84,14 +102,16 @@ function createCube(x, y, z) {
         0, 0, 0,
         0, 0, 1,
         1, 0, 0,
-        1, 0, 1,
-    )
+        1, 0, 1]
+    , colorsOffset)
 
-    let idx = (meshTriangles.length << 1) / 3
+    colorsOffset += 72
 
-    meshTriangles.push(
+    let idx = trianglesLen << 1
+
+    triangles.set(
         //Frente
-        idx + 0, idx + 1, idx + 2,
+        [idx + 0, idx + 1, idx + 2,
         idx + 1, idx + 3, idx + 2,
 
         //Trás
@@ -112,15 +132,20 @@ function createCube(x, y, z) {
 
         //Baixo
         idx + 20, idx + 21, idx + 22,
-        idx + 21, idx + 23, idx + 22
-    )
+        idx + 21, idx + 23, idx + 22]
+    , trianglesOffset)
+
+    trianglesOffset += 36
+
+    trianglesLen += 12
 }
 
 for (let i = 0; i < 20; i++) {
     for (let j = 0; j < 20; j++) {
         for (let k = 0; k < 20; k++) {
             if (i === 0 || i === 19 || j === 0 || j === 19 || k === 19)
-            createCube(i, k, j)
+                createCube(i, k, j)
         }
     }
 }
+
